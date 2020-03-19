e98b372ab16410de192c9392b9558f112a32b6d5
Maël Nison <nison.mael@gmail.com>
2020-03-19 21:27:59 +0100

    Update gatsby-ssr.js

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -3,54 +3,68 @@ import { stripIndent } from "common-tags";
 
 exports.onRenderBody = (
   { setHeadComponents, setPostBodyComponents },
-  { apiKey, indexName, inputSelector, debug = false }
+  opts
 ) => {
-  if (!apiKey || !indexName || !inputSelector) {
-    return;
-  }
-
-  setHeadComponents([
+  const headComponents = [
     <link
       key="plugin-docsearch-css"
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
     />
-  ]);
-
-  setPostBodyComponents([
+  ];
+  
+  const postBodyComponents = [
     <script
       key="plugin-docsearch-js"
       type="text/javascript"
       src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
     />,
-    <script
-      key="plugin-docsearch-initiate"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: stripIndent`
-        var observer = new MutationObserver(function (mutations, instance) {
-          var docuSearchElem = document.querySelector('${inputSelector}');
-          if (docuSearchElem) {
-            docsearch({
-              apiKey: "${apiKey}",
-              indexName: "${indexName}",
-              inputSelector: "${inputSelector}",
-              debug: ${debug === true ? "true" : "false"}
+  ];
+  
+  const addSpec = spec => {
+    postBodyComponents.push(
+      <script
+        key="plugin-docsearch-initiate"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: stripIndent`
+            var observer = new MutationObserver(function (mutations, instance) {
+              var docuSearchElem = document.querySelector('${inputSelector}');
+              if (docuSearchElem) {
+                docsearch({
+                  apiKey: "${spec.apiKey}",
+                  indexName: "${spec.indexName}",
+                  inputSelector: "${spec.inputSelector}",
+                  debug: ${spec.debug === true ? "true" : "false"}
+                });
+                instance.disconnect(); // stop observing
+                return;
+              }
             });
-            instance.disconnect(); // stop observing
-            return;
-          }
-        });
 
-        // start observing
-        document.addEventListener("DOMContentLoaded", function() {
-          observer.observe(document, {
-            childList: true,
-            subtree: true
-          });
-        });
-        `
-      }}
-    />
-  ]);
+            // start observing
+            document.addEventListener("DOMContentLoaded", function() {
+              observer.observe(document, {
+                childList: true,
+                subtree: true
+              });
+            });
+          `
+        }}
+      />
+    );
+  };
+  
+  const specs = Array.isArray(opts)
+    ? specs
+    : [specs];
+
+  for (const spec of specs) {
+    if (spec.apiKey && spec.indexName && spec.inputSelector) {
+      addSpec(spec);
+    }
+  }
+
+  setHeadComponents(headComponents);
+  setPostBodyComponents(postBodyComponents);
 };
